5a8d15e38f9ca107fba48f90d1e5c3f901b0b954
marinheiro <1241034@isep.ipp.pt>
2025-10-25 14:21:14 +0100

    resetei

---
+++ cmake-build-debug/CoreTests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ruima/Documents/ESOFT/leti-esoft-25-26-A3/CoreTests/lib/googletest
+# Build directory: C:/Users/ruima/Documents/ESOFT/leti-esoft-25-26-A3/cmake-build-debug/CoreTests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- project-template/ConsoleApp/ConsoleApp.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    std::wcout << L"Console Application!" << std::endl;
-    return 0;
-}
